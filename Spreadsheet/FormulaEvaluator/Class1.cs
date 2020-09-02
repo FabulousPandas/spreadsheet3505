a919b24627f812e7f3a9bb4e2782341d95c513e8
FabulousPandas <khristha@gmail.com>
2020-09-02 17:27:47 -0600

    Added comments to the class and the Evaluate() function

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -2,8 +2,26 @@
 
 namespace FormulaEvaluator
 {
+    /// <summary>
+    /// Evaluates integer infix expressions written using standard infix notation
+    /// </summary>
     public static class Evaluator
     {
-        // TODO: Follow the PS1 instructions
+        //Delegate for describing how the values for variables are looked up
+        public delegate int Lookup(String v);
+
+        /// <summary>
+        /// Evaluates the given integer infix expression and returns the result
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="variableEvaluator"></param>
+        /// <returns></returns>
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+
+
+            return 0;
+        }
+
     }
 }
