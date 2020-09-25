81ee5c5020f12db8e61d6938010f1bdd6925065a
FabulousPandas <khristha@gmail.com>
2020-09-25 13:29:12 -0600

    Removed unnecessary comments

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -43,11 +43,6 @@ namespace SS
             return Regex.IsMatch(var, pattern);
         }
 
-        /// <summary>
-        /// If name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, returns the contents (as opposed to the value) of the named cell.  The return
-        /// value should be either a string, a double, or a Formula.
         public override object GetCellContents(string name)
         {
             if (name == null || !IsVar(name))
@@ -57,24 +52,11 @@ namespace SS
             return cells[name];
         }
 
-        /// <summary>
-        /// Enumerates the names of all the non-empty cells in the spreadsheet.
-        /// </summary>
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
             return cells.Keys;
         }
 
-        /// <summary>
-        /// If name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, the contents of the named cell becomes number.  The method returns a
-        /// list consisting of name plus the names of all other cells whose value depends, 
-        /// directly or indirectly, on the named cell.
-        /// 
-        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
-        /// list {A1, B1, C1} is returned.
-        /// </summary>
         public override IList<string> SetCellContents(string name, double number)
         {
             if (name == null || !IsVar(name))
@@ -84,18 +66,6 @@ namespace SS
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// If text is null, throws an ArgumentNullException.
-        /// 
-        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, the contents of the named cell becomes text.  The method returns a
-        /// list consisting of name plus the names of all other cells whose value depends, 
-        /// directly or indirectly, on the named cell.
-        /// 
-        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
-        /// list {A1, B1, C1} is returned.
-        /// </summary>
         public override IList<string> SetCellContents(string name, string text)
         {
             if (text == null)
@@ -109,21 +79,6 @@ namespace SS
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// If the formula parameter is null, throws an ArgumentNullException.
-        /// 
-        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
-        /// 
-        /// Otherwise, if changing the contents of the named cell to be the formula would cause a 
-        /// circular dependency, throws a CircularException, and no change is made to the spreadsheet.
-        /// 
-        /// Otherwise, the contents of the named cell becomes formula.  The method returns a
-        /// list consisting of name plus the names of all other cells whose value depends,
-        /// directly or indirectly, on the named cell.
-        /// 
-        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
-        /// list {A1, B1, C1} is returned.
-        /// </summary>
         public override IList<string> SetCellContents(string name, Formula formula)
         {
             if (formula == null)
@@ -137,19 +92,6 @@ namespace SS
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Returns an enumeration, without duplicates, of the names of all cells whose
-        /// values depend directly on the value of the named cell.  In other words, returns
-        /// an enumeration, without duplicates, of the names of all cells that contain
-        /// formulas containing name.
-        /// 
-        /// For example, suppose that
-        /// A1 contains 3
-        /// B1 contains the formula A1 * A1
-        /// C1 contains the formula B1 + A1
-        /// D1 contains the formula B1 - C1
-        /// The direct dependents of A1 are B1 and C1
-        /// </summary>
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
             return graph.GetDependents(name);
