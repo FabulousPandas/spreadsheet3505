57b77dad2e41c862b1c261e0b16499972133da10
Khris Thammavong <khristha@gmail.com>
2021-04-16 18:50:22 -0600

    Added networking code from Tank Wars project

---
+++ Spreadsheet/NetworkController/Networking.cs
@@ -0,0 +1,322 @@
+﻿/// Authors: Ervin Chhour and Khris Thammavong
+
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkUtil
+{
+
+    public static class Networking
+    {
+        /// <summary>
+        /// Private helper method for calling the OnNetworkAction function after an error has occurred
+        /// </summary>
+        /// <param name="toCall"></param>
+        /// <param name="errorMessage"></param>
+        private static void ErrorOccurred(Action<SocketState> toCall, string errorMessage)
+        {
+            SocketState ss = new SocketState(toCall, null);
+            ss.ErrorOccured = true;
+            ss.ErrorMessage = errorMessage;
+            ss.OnNetworkAction(ss);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////
+        // Client-Side Code
+        /////////////////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Begins the asynchronous process of connecting to a server via BeginConnect, 
+        /// and using ConnectedCallback as the method to finalize the connection once it's made.
+        /// 
+        /// If anything goes wrong during the connection process, toCall should be invoked 
+        /// with a new SocketState with its ErrorOccured flag set to true and an appropriate message 
+        /// placed in its ErrorMessage field. Between this method and ConnectedCallback, toCall should 
+        /// only be invoked once on error.
+        ///
+        /// This connection process should timeout and produce an error (as discussed above) 
+        /// if a connection can't be established within 3 seconds of starting BeginConnect.
+        /// 
+        /// </summary>
+        /// <param name="toCall">The action to take once the connection is open or an error occurs</param>
+        /// <param name="hostName">The server to connect to</param>
+        /// <param name="port">The port on which the server is listening</param>
+        public static void ConnectToServer(Action<SocketState> toCall, string hostName, int port)
+        {
+            // Establish the remote endpoint for the socket.
+            IPHostEntry ipHostInfo;
+            IPAddress ipAddress = IPAddress.None;
+
+            // Determine if the server address is a URL or an IP
+            try
+            {
+                ipHostInfo = Dns.GetHostEntry(hostName);
+                bool foundIPV4 = false;
+                foreach (IPAddress addr in ipHostInfo.AddressList)
+                    if (addr.AddressFamily != AddressFamily.InterNetworkV6)
+                    {
+                        foundIPV4 = true;
+                        ipAddress = addr;
+                        break;
+                    }
+                // Didn't find any IPV4 addresses
+                if (!foundIPV4)
+                {
+                    ErrorOccurred(toCall, "Could not find any IPV4 Addresses");
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                // see if host name is a valid ipaddress
+                try
+                {
+                    ipAddress = IPAddress.Parse(hostName);
+                }
+                catch (Exception)
+                {
+                    ErrorOccurred(toCall, "Host name is not a valid ip address");
+                    return;
+                }
+            }
+
+            try
+            {
+                // Create a TCP/IP socket.
+                Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                // This disables Nagle's algorithm (google if curious!)
+                // Nagle's algorithm can cause problems for a latency-sensitive 
+                // game like ours will be 
+                socket.NoDelay = true;
+
+                // TODO: Finish the remainder of the connection process as specified.
+                SocketState ss = new SocketState(toCall, socket);
+
+                IAsyncResult result = ss.TheSocket.BeginConnect(ipAddress, port, ConnectedCallback, ss);
+
+                bool success = result.AsyncWaitHandle.WaitOne(3000);
+
+                if (!success)
+                {
+                    ss.TheSocket.Close(); //closes the socket if the connection times out
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred(toCall, e.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// To be used as the callback for finalizing a connection process that was initiated by ConnectToServer.
+        ///
+        /// Uses EndConnect to finalize the connection.
+        /// 
+        /// As stated in the ConnectToServer documentation, if an error occurs during the connection process,
+        /// either this method or ConnectToServer (not both) should indicate the error appropriately.
+        /// 
+        /// If a connection is successfully established, invokes the toCall Action that was provided to ConnectToServer (above)
+        /// with a new SocketState representing the new connection.
+        /// 
+        /// </summary>
+        /// <param name="ar">The object asynchronously passed via BeginConnect</param>
+        private static void ConnectedCallback(IAsyncResult ar)
+        {
+            SocketState ss = (SocketState)ar.AsyncState;
+            try
+            {
+                ss.TheSocket.EndConnect(ar);
+                ss.OnNetworkAction(ss);
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred(ss.OnNetworkAction, e.Message);
+            }
+        }
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////
+        // Server and Client Common Code
+        /////////////////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Begins the asynchronous process of receiving data via BeginReceive, using ReceiveCallback 
+        /// as the callback to finalize the receive and store data once it has arrived.
+        /// The object passed to ReceiveCallback via the AsyncResult should be the SocketState.
+        /// 
+        /// If anything goes wrong during the receive process, the SocketState's ErrorOccured flag should 
+        /// be set to true, and an appropriate message placed in ErrorMessage, then the SocketState's
+        /// OnNetworkAction should be invoked. Between this method and ReceiveCallback, OnNetworkAction should only be 
+        /// invoked once on error.
+        /// 
+        /// </summary>
+        /// <param name="state">The SocketState to begin receiving</param>
+        public static void GetData(SocketState state)
+        {
+            try
+            {
+                state.TheSocket.BeginReceive(state.buffer, 0, state.buffer.Length, SocketFlags.None, ReceiveCallback, state);
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred(state.OnNetworkAction, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// To be used as the callback for finalizing a receive operation that was initiated by GetData.
+        /// 
+        /// Uses EndReceive to finalize the receive.
+        ///
+        /// As stated in the GetData documentation, if an error occurs during the receive process,
+        /// either this method or GetData (not both) should indicate the error appropriately.
+        /// 
+        /// If data is successfully received:
+        ///  (1) Read the characters as UTF8 and put them in the SocketState's unprocessed data buffer (its string builder).
+        ///      This must be done in a thread-safe manner with respect to the SocketState methods that access or modify its 
+        ///      string builder.
+        ///  (2) Call the saved delegate (OnNetworkAction) allowing the user to deal with this data.
+        /// </summary>
+        /// <param name="ar"> 
+        /// This contains the SocketState that is stored with the callback when the initial BeginReceive is called.
+        /// </param>
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            SocketState ss = (SocketState)ar.AsyncState;
+            try
+            {
+                int numBytes = ss.TheSocket.EndReceive(ar);
+                if (numBytes == 0)
+                {
+                    ErrorOccurred(ss.OnNetworkAction, "Socket is cleanly shut down");
+                    return;
+                }
+                string data = Encoding.UTF8.GetString(ss.buffer, 0, numBytes);
+
+                lock (ss.data)
+                {
+                    ss.data.Append(data);
+                }
+
+                ss.OnNetworkAction(ss);
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred(ss.OnNetworkAction, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Begin the asynchronous process of sending data via BeginSend, using SendCallback to finalize the send process.
+        /// 
+        /// If the socket is closed, does not attempt to send.
+        /// 
+        /// If a send fails for any reason, this method ensures that the Socket is closed before returning.
+        /// </summary>
+        /// <param name="socket">The socket on which to send the data</param>
+        /// <param name="data">The string to send</param>
+        /// <returns>True if the send process was started, false if an error occurs or the socket is already closed</returns>
+        public static bool Send(Socket socket, string data)
+        {
+            try
+            {
+                byte[] dataArray = Encoding.ASCII.GetBytes(data);
+                socket.BeginSend(dataArray, 0, dataArray.Length, SocketFlags.None, SendCallback, socket);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                socket.Close();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// To be used as the callback for finalizing a send operation that was initiated by Send.
+        ///
+        /// Uses EndSend to finalize the send.
+        /// 
+        /// This method must not throw, even if an error occured during the Send operation.
+        /// </summary>
+        /// <param name="ar">
+        /// This is the Socket (not SocketState) that is stored with the callback when
+        /// the initial BeginSend is called.
+        /// </param>
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = (Socket) ar.AsyncState;
+                socket.EndSend(ar);
+            }
+            catch(Exception)
+            {
+            }
+        }
+
+
+        /// <summary>
+        /// Begin the asynchronous process of sending data via BeginSend, using SendAndCloseCallback to finalize the send process.
+        /// This variant closes the socket in the callback once complete. This is useful for HTTP servers.
+        /// 
+        /// If the socket is closed, does not attempt to send.
+        /// 
+        /// If a send fails for any reason, this method ensures that the Socket is closed before returning.
+        /// </summary>
+        /// <param name="socket">The socket on which to send the data</param>
+        /// <param name="data">The string to send</param>
+        /// <returns>True if the send process was started, false if an error occurs or the socket is already closed</returns>
+        public static bool SendAndClose(Socket socket, string data)
+        {
+            if (!socket.Connected)
+                return false;
+
+            try
+            {
+                byte[] dataArray = Encoding.ASCII.GetBytes(data);
+                socket.BeginSend(dataArray, 0, dataArray.Length, SocketFlags.None, SendAndCloseCallback, socket);
+
+                return true;
+            }
+            catch
+            {
+                socket.Close();
+                return false;
+            }        
+        }
+
+        /// <summary>
+        /// To be used as the callback for finalizing a send operation that was initiated by SendAndClose.
+        ///
+        /// Uses EndSend to finalize the send, then closes the socket.
+        /// 
+        /// This method must not throw, even if an error occured during the Send operation.
+        /// 
+        /// This method ensures that the socket is closed before returning.
+        /// </summary>
+        /// <param name="ar">
+        /// This is the Socket (not SocketState) that is stored with the callback when
+        /// the initial BeginSend is called.
+        /// </param>
+        private static void SendAndCloseCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = (Socket)ar.AsyncState;
+                socket.EndSend(ar);
+                socket.Close();
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+    }
+}
