d45b4e1b19745775319b275d00e880cffec34b9d
Ervin-Chhour <Snivyleafy@gmail.com>
2021-04-23 17:23:04 -0600

    Client request and Server message classes added

---
+++ Spreadsheet/SpreadsheetController/ServerMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetController
+{
+    public class ServerMessage
+    {
+        public String cellName;
+        public String contents;
+        public String message;
+        public String selector;
+        public String selectorName;
+    }
+}
